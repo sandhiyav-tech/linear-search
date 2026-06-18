5faf830e24d2e5db4857ea51a3de799d6113da71
sandhiyav-tech <sundiya577@gmail.com>
2026-06-18 10:47:41 +0530

    Add files via upload

---
+++ program2.py
@@ -0,0 +1,17 @@
+def binary_search(arr,target):
+    low=0
+    high=len(arr)-1
+    while low<=high:
+        mid=(low+high)//2
+        if arr[mid] == target:
+            return mid
+        elif arr[mid] < target:
+            low=mid+1
+        else:
+            high=mid-1
+    return -1
+num = [3,4,5,6,7]
+target = 5
+result = binary_search(num,target)
+print(result)
+
